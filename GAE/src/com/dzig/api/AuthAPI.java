ff5668e91ca30fe0d075b153838fcd6fc6814a38
Konstantin Tarasenko <konstantin.v.tarasenko@gmail.com>
2012-12-11 01:05:31 +0200

    fixed GET/POST parsing for auth call

--- GAE/src/com/dzig/api/AuthAPI.java
+++ GAE/src/com/dzig/api/AuthAPI.java
@@ -15,15 +15,26 @@ public class AuthAPI extends ServerResource {
 
 	@SuppressWarnings("unused")
 	private static final Logger log = Logger.getLogger("API");
-	
-	@Get
+
 	@Post
 	public void handlePost(Representation rep) throws JSONException{
 		Form form = new Form(rep);
+		doAuth(form.getFirstValue("token"), form.getFirstValue("continueUrl"), form.getFirstValue("method"));
+	}
+	
+	@Get
+	public void handleGet(Representation rep){
+		doAuth(getQueryValue("token"),getQueryValue("continueUrl"), getQueryValue("method"));
+	}
+	
+	
+	public void doAuth(String token, String continueUrl, String method){
 		String url;
-		String token = form.getFirstValue("token");
-		String continueUrl = form.getFirstValue("continueUrl", getRootRef() + "/user");
-		if ("logout".equals(form.getFirstValue("method"))){
+		if (continueUrl == null){
+			continueUrl = getRootRef() + "/user";
+		}
+		System.out.println(token + " " + continueUrl + " " + method);
+		if ("logout".equals(method)){
 			url = UserServiceFactory.getUserService().createLogoutURL(continueUrl);
 		} else {
 			if (token != null){
